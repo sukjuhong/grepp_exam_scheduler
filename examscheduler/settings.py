0f5e7018c01ab229fd2b87398946caf46cb3e7b3
Sukju Hong <ehensnfl@gmail.com>
2025-03-15 18:01:17 +0000

    fix: adjust AUTHENTICATION_CLASSES order

--- examscheduler/settings.py
+++ examscheduler/settings.py
@@ -141,8 +141,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10
