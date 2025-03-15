51ca8b64a70fdc8fff5735a6f698481eb410aea0
Sukju Hong <ehensnfl@gmail.com>
2025-03-15 15:55:37 +0000

    feat: add reservations admin view

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from reservations.models import Reservation
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'start_time',
+                    'end_time', 'customer', 'status']
+    list_filter = ('customer', 'date', 'status')
+    search_fields = ('customer', 'date', 'status')
+    ordering = ('-date', 'start_time')
+
+
+admin.site.register(Reservation, ReservationAdmin)
